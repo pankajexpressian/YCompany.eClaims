bbda73fa73df9337a57cd46d91086a145e6c564e
Pankaj Jangid - N <pankajexpressian@gmail.com>
2022-06-06 12:52:16 +0530

    Added service aggregator empty project

---
+++ src/Gateways/ServiceAggregator/ServiceAggregator.API/Controllers/ServiceAggregatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceAggregator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceAggregatorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Aggregator Service");
+        }
+    }
+}
